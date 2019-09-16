e543919c3c8c4fa640f846c9e407b4fbabdf0566
Jonathan Freitas <freitas.jonathan08@gmail.com>
2019-09-15 23:39:36 -0300

    ajuste

--- IPTVAdmin/billet/admin.py
+++ IPTVAdmin/billet/admin.py
@@ -9,5 +9,5 @@ class BilletAdmin(admin.ModelAdmin):
     exclude = ['deleted_at']
     list_display = ['id', 'profile', 'dueDate', 'updated_at', 'created_at']
     list_display_links = ['id', 'profile']
-    readonly_fields = ['profile', 'code', 'reference', 'numberOfPayments', 'instructions',
+    readonly_fields = ['profile', 'code', 'paymentLink', 'reference', 'numberOfPayments', 'instructions',
                        'description', 'amount']
