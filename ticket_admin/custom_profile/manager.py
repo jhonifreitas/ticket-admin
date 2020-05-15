c0c470820a8ebb1c1f5b3a3c889b5b889d993ae6
jonathan freitas <jonathan@MacBook-Pro-de-jonathan.local>
2020-05-15 19:19:33 -0300

    ajuste

--- ticket_admin/custom_profile/manager.py
+++ ticket_admin/custom_profile/manager.py
@@ -9,23 +9,22 @@ class ProfileUserManager(Manager):
     MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     def get_billing(self, user):
-        queryset = self.filter(
+        result = self.filter(
             Q(status=models.ProfileUser.ACTIVE) |
             Q(status=models.ProfileUser.WAITING),
             profile__user=user
-        ).values('value').annotate(total=Sum('value')).order_by()
-        if queryset:
-            return queryset[0].get('total')
+        ).aggregate(total=Sum('value')).get('total')
+        if result:
+            return result
         return 0
 
     def get_billing_liquid(self, user):
-        query_user = self.filter(
+        user_total = self.filter(
             Q(status=models.ProfileUser.ACTIVE) |
             Q(status=models.ProfileUser.WAITING),
             profile__user=user
-        ).values('value').annotate(total=Sum('value')).order_by()
-        query_panel = self.filter(
-            profile__user=user).values('panel').annotate(total=Sum('panel__value')).order_by()
-        if query_user and query_user:
-            return query_user[0].get('total') - query_panel[0].get('total')
+        ).aggregate(total=Sum('value')).get('total')
+        panel_total = self.filter(profile__user=user).aggregate(total=Sum('panel__value')).get('total')
+        if user_total and panel_total:
+            return user_total - panel_total
         return 0
