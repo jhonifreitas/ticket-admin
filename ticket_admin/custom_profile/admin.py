8da245e962163faf02b3ae03ad21446450d0dba6
jonathan freitas <jonathan@MacBook-Pro-de-jonathan.local>
2020-05-21 14:10:12 -0300

    hidden phone and password

--- ticket_admin/custom_profile/admin.py
+++ ticket_admin/custom_profile/admin.py
@@ -6,14 +6,40 @@ from ticket_admin.custom_profile import models
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
 
-    exclude = ['deleted_at']
-    list_display = ['id', 'user', 'name', 'phone', 'updated_at', 'created_at']
+    exclude = ['deleted_at', 'phone', 'cpf', 'email']
+    list_display = ['id', 'user', 'name', 'get_phone', 'updated_at', 'created_at']
     list_display_links = ['id', 'user']
+    readonly_fields = ['user', 'get_phone', 'get_cpf', 'get_email']
+
+    def get_phone(self, obj):
+        part_one = obj.phone[:3]
+        part_two = '*'*len(obj.phone[3:])
+        return '{}{}'.format(part_one, part_two)
+    get_phone.short_description = 'Telefone'
+
+    def get_email(self, obj):
+        part_one = obj.email[:3]
+        part_two = '*'*len(obj.email[3:])
+        return '{}{}'.format(part_one, part_two)
+    get_email.short_description = 'Email'
+
+    def get_cpf(self, obj):
+        part_one = obj.cpf[:3]
+        part_two = '*'*len(obj.cpf[3:])
+        return '{}{}'.format(part_one, part_two)
+    get_cpf.short_description = 'CPF'
 
 
 @admin.register(models.ProfileUser)
 class ProfileUserAdmin(admin.ModelAdmin):
 
-    exclude = ['deleted_at']
-    list_display = ['id', 'profile', 'username', 'password', 'status', 'updated_at', 'created_at']
+    exclude = ['deleted_at', 'password']
+    list_display = ['id', 'profile', 'username', 'get_password', 'status', 'updated_at', 'created_at']
     list_display_links = ['id', 'profile']
+    readonly_fields = ['get_password', 'profile']
+
+    def get_password(self, obj):
+        part_one = obj.password[:3]
+        part_two = '*'*len(obj.password[3:])
+        return '{}{}'.format(part_one, part_two)
+    get_password.short_description = 'Senha'
