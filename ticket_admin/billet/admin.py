9d1dfeaf44fb92a3a9914cf5388be6c3d2002090
jonathan freitas <jonathan@MacBook-Pro-de-jonathan.local>
2020-06-13 12:28:15 -0300

    ajuste

--- ticket_admin/billet/admin.py
+++ ticket_admin/billet/admin.py
@@ -7,7 +7,11 @@ from ticket_admin.billet import models
 class BilletAdmin(admin.ModelAdmin):
 
     exclude = ['deleted_at']
-    list_display = ['id', 'profile', 'dueDate', 'status', 'created_at']
-    list_display_links = ['id', 'profile']
+    list_display = ['id', 'dueDate', 'get_user', 'profile', 'status', 'created_at']
+    list_display_links = ['id', 'dueDate']
     readonly_fields = ['profile', 'code', 'paymentLink', 'reference', 'numberOfPayments', 'instructions',
                        'description', 'amount']
+
+    def get_user(self, obj):
+        return obj.profile.user
+    get_user.short_description = 'Usuário'
